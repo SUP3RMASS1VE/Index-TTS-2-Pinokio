7c5776ee835af77d7980a6c76d2355fa6d6f216a
SUP3RMASS1VE <Martincull@live.com>
2025-09-13 13:21:54 +0100

    Update install.js

--- install.js
+++ install.js
@@ -28,6 +28,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
+          "conda install -c conda-forge pynini==2.1.6 -y",
+          "uv pip install WeTextProcessing --no-deps",
           "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt"
         ]
